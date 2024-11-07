0e5ffc15690b6f02fb2559e01d3a7ff3226a42b3
DripTrace <rpalm@driptrace.io>
2024-11-06 23:22:13 -0600

    prod setting again

--- components/UserButton.tsx
+++ components/UserButton.tsx
@@ -21,7 +21,6 @@ import LoadingSpinner from "./loadingSpinner";
 
 function UserButton({ session }: { session: Session | null }) {
 	const subscription = useSubscriptionStore((state) => state.subscription);
-	// console.log("SESSION (UserButton.tsx): ", session);
 
 	// Session ...
 	if (!session)
